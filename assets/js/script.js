555621768e2096c07b775a7267b41db184a77226
Andrew Ogilvie <primalorb@gmail.com>
2021-03-26 19:59:18 -0400

    add forecast data from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,6 +71,7 @@ function fetchWeatherData( geocode ) {
         if (response.ok) {
           response.json().then(function(data) {
             postCurrent( data )
+            postForecast( data )
           });
         } else {
           alert("Error: " + response.statusText);
@@ -83,38 +84,102 @@ function fetchWeatherData( geocode ) {
 
 // post location ( city / country ) to title
 function postLocation( data ) {
-    $( '.location' )
+    var location =  $( '<h2>' )
+        .addClass( 'location' )    
         .text( `${ data.name }, ${ data.country }` )
+    $( '.current-title' )
+        .html( '' )
+        .append( location )
 }
 
 function postCurrent( data ) {
-    console.log(data)
-        // get current time
     var datetime = data.current.dt
         // create moment item
     var momentTime = moment( ( datetime ) * 1000 ).format('dddd MMMM Do, YYYY' )
         // set data in title
-    $( '.date' )
+    var date =  $( '<h2>' )
+        .addClass( 'date' )    
         .text( `( ${ momentTime } )` )
+    $( '.current-title' )
+        .append( date )
+    var iconImg = $( '<img>' )
+        .attr( 'src', `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`)
+    var iconDiv = $( '<div>' )
+        .addClass( 'icon' )
+        .append( iconImg  )
+    $( '.current-icon' )
+        .append( iconDiv )
 
-    $( '#cur-temperature' )
-        .text( `${(data.current.temp).toFixed(1)} °C` )
-    $( '#cur-humidity' )
+    var tempSpan = $( '<span> ')
+        .text( `${(data.current.temp).toFixed(1)} °C` ) 
+    var humiditySpan = $( '<span>' )
         .text( `${data.current.humidity} %` )
-    $( '#cur-wind' )
+    var windSpan = $( '<span>' )
         .text( `${( data.current.wind_speed * 3.6 ).toFixed(1)} km/h` )
-    $( '#cur-wind-dir' )
+    var windArrow = $( '<span>' )
         .html( '<i class="fas fa-location-arrow"></i>' )
-        .css({ 'transform' : 'rotate('+ ( -45 + data.current.wind_deg ) +'deg)' })
-    $( '#cur-uv' )
+        .css({ 'transform' : 'rotate('+ ( 135 + data.current.wind_deg ) +'deg)' })
+    var uvSpan = $( '<span>' )
         .text( data.current.uvi )
         .css({ 'background-color': `hsl(${ uvColor( data.current.uvi ) },  100%, 50%)` })
 
-
-     var test = $( '#cur-wind' )
-        .closest( 'i' )
+    $( '#cur-temperature' )
+        .append( tempSpan ) 
+    $( '#cur-humidity' )
+        .append( humiditySpan )
+    $( '#cur-wind' )
+        .append( windSpan )
+        .append( windArrow )
+    $( '#cur-uv' )
+        .append( uvSpan )
 }
 
+function postForecast( data ) {
+    console.log( data )
+    $( '.card' )
+        .each( function( i ) {
+            var datetime = data.daily[i+1].dt
+                // create moment item
+            var momentTime = moment( ( datetime ) * 1000 ).format('MM/DD/YY' )
+                // title of the date
+            var title = $( '<h3>' )
+                .text( momentTime ) 
+
+                // icon
+            var iconImg = $( '<img>' )
+                .attr( 'src', `http://openweathermap.org/img/wn/${data.daily[i+1].weather[0].icon}@2x.png`)
+            var iconDiv = $( '<div>' )
+                .addClass( 'icon' )
+                .append( iconImg  )
+                // dislay daytime temperature
+            var tempTitle = $( '<span>' )
+                .text( 'Temp: ')    
+            var tempSpan = $( '<span> ')
+                .text( `${(data.daily[i+1].temp.day).toFixed(1)} °C` )
+            var tempDiv = $( '<div>' )
+                .append( tempTitle )
+                .append( tempSpan )
+                // dislay humidity
+            var humdityTitle = $( '<span>' )
+                .text( 'Humidity: ')    
+            var humiditySpan = $( '<span> ')
+                .text( `${(data.daily[i+1].humidity).toFixed(1)} %` ) 
+            var humidityDiv = $( '<div>' )
+                .append( humdityTitle )
+                .append( humiditySpan )
+
+                // append elements    
+            $( this )
+            .append( title )
+            .append( iconDiv )
+            .append( tempDiv )
+            .append( humidityDiv )
+
+        })
+        
+}
+
+// set angle of HSL wheel for apply for UV severity color
 function uvColor( value ) {
         // get percentage of UV scale
     var scaleUV = value / 11    
@@ -130,6 +195,6 @@ function uvColor( value ) {
 
 
 
-
-geoLocation()
+    // Initialize current date pull
+geoLocation();
 
